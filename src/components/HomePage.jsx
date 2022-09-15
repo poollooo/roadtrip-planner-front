61f4ff2d3c9df1fe149d1fc7a083647d7f09776f
Paul Nizet <polonizet@gmail.com>
2022-09-15 17:18:06 +0200

    add image and search bar to homepage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import HeroImage from "../images/Hero-Image.svg";
+import SearchBar from "./SearchBar";
 import DisplayCards from "./DisplayCards";
 
 const HomePage = () => {
   return (
-    <>
+    <div className="flex flex-row justify-center py-8">
+      <div className="items-center">
+        <img src={HeroImage} alt="logo" className="w-[80vw]" />
+        <SearchBar />
+      </div>
+    </div>
       <div className="flex flex-col align-center gap-20">
         <div className="flex flex-col gap-10">
           <DisplayCards trip={true} headerContent={"My trips"}></DisplayCards>
@@ -24,7 +31,6 @@ const HomePage = () => {
           ></DisplayCards>
         </div>
       </div>
-    </>
   );
 };
 
