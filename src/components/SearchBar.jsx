d58628b8768224167f3a4bcbfcbd553837a0e063
Paul Nizet <polonizet@gmail.com>
2022-09-15 17:19:00 +0200

    add search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <div className='flex flex-row justify-between items-center mx-20 h-20 bg-white border-solid border-[2px] border-green-pine rounded-md text-black w-[70vw]'>
+            <div className='flex flex-col justify-between pl-4 w-[20vw] gap-2'>
+                <h1>Search a city</h1>
+                <input type="text" id="search-city" name="search-city" placeholder='Paris, Katmandhu, Cancun?' className='focus:outline-none' />
+            </div>
+            <div className='flex flex-col justify-between'>
+                <p>date picker component goes here</p>
+            </div>
+            <button className='bg-green-pine justify-center text-white my-auto w-[16vw] h-full'>Start Exploring</button>
+        </div>
+    )
+}
+
+export default SearchBar
