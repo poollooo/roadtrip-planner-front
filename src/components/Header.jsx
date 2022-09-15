f687e1aaf765038137ca479b29eba4deed74757c
YUAN LIU <yuanliu0120@gmail.com>
2022-09-15 15:39:16 +0200

    Create-component-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,37 @@
 import React from "react";
+import logo from "../images/Logo.svg";
+import { Link } from "react-router-dom";
+import profilePic from "../images/ProfilePicture.svg";
 
 const Header = () => {
   return (
-    <div className="flex flex-row justify-around bg-green-pine">
-      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
-      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
-      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
-      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
-    </div>
-  )
+    <header className="flex flex-row justify-between bg-white">
+      <img src={logo} alt="Road-Trip-Planner" className="w-40 h-auto my-8 mx-8" />
+      <nav className="flex flex-row justify-between items-center text-center ">
+        <ul className="flex flex-row justify-between ">
+          <li className="mx-4">
+            <Link className="hover:text-green-pine">
+              <p>Home</p>
+            </Link>
+          </li>
+          <li className="mx-4">
+            <Link className="hover:text-green-pine">
+              <p>About</p>
+            </Link>
+          </li>
+          <li className="mx-4">
+            <Link className="hover:text-green-pine">
+              <p>My Trips</p>
+            </Link>
+          </li>
+        </ul>
+
+        <Link>
+          <img src={profilePic} alt="ProfilePicture" className="mx-16" />
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,13 @@ import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <>
+    <div className="mx-16">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
