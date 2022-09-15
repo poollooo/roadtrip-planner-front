91c51f23cab00e1374cf5731ffc0421434c4a7d5
Paul Nizet <polonizet@gmail.com>
2022-09-15 14:39:40 +0200

    set up tailwind css with custom color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex flex-row justify-around bg-green-pine">
+      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
+      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
+      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
+      <h1 className="flex flex-col text-center text-white">Headerrrr</h1>
+    </div>
+  )
 };
 
 export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          pine: "#03666B",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
