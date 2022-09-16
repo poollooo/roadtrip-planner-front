02adb81476521492ef53fe770a21fc13e53c0d32
Paul Nizet <polonizet@gmail.com>
2022-09-16 12:23:19 +0200

    remove useless css

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = () => {
             <div className='flex flex-col justify-center pl-4 w-[20vw]'>
                 <input type="text" id="search-city" name="search-city" placeholder='Search a city' className='focus:outline-none text-lg' />
             </div>
-            <div className='flex flex-col justify-between w-[30vw]'>
+            <div className='w-[30vw]'>
                 <BasicDateRangePicker />
             </div>
             <button className='bg-green-pine justify-center lg:text-[1.5vw] text-[2vw] text-white w-[16vw] h-full rounded-tr-2xl rounded-br-2xl'>Start Exploring</button>
