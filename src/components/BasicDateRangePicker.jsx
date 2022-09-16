8bfe1c0b00fb1a159cd34c37b62c29b6b3fca897
Paul Nizet <polonizet@gmail.com>
2022-09-16 17:02:41 +0200

    remove props and add QueryContext on DatePicker

--- src/components/BasicDateRangePicker.jsx
+++ src/components/BasicDateRangePicker.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-
+import React, { useContext, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-
 import { formatAs } from '../utils/formatDate'
+import QueryContext from '../Context/QueryContext';
 
-export default function BasicDateRangePicker({ searchQuery, setSearchQuery }) {
+export default function BasicDateRangePicker() {
     const [value, setValue] = useState([null, null]);
+    const { searchQuery, setSearchQuery } = useContext(QueryContext);
 
     return (
         <LocalizationProvider
@@ -23,8 +23,8 @@ export default function BasicDateRangePicker({ searchQuery, setSearchQuery }) {
                     // the start date and end date in the searchQuery
                     setSearchQuery({
                         ...searchQuery,
-                        startDate: formatAs.yearMonthDate(newValue[0].$d),
-                        endDate: formatAs.yearMonthDate(newValue[1].$d),
+                        startDate: formatAs.yearMonthDate(newValue[0]?.$d),
+                        endDate: formatAs.yearMonthDate(newValue[1]?.$d),
                     })
                 }}
                 renderInput={(startProps, endProps) => (
