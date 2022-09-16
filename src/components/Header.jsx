e5d3fc9bc1c59d6a82b598abd274adaaef1c3983
YUAN LIU <yuanliu0120@gmail.com>
2022-09-16 12:20:23 +0200

    Layout-responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,34 @@ import profilePic from "../images/ProfilePicture.svg";
 const Header = () => {
   return (
     <header className="flex flex-row justify-between bg-white">
-      <img src={logo} alt="Road-Trip-Planner" className="w-40 h-auto my-8 mx-8" />
+      <img
+        src={logo}
+        alt="Road-Trip-Planner"
+        className="w-20 sm:w-30 md:w-40 lg:w-40 h-auto my-8 mx-8"
+      />
       <nav className="flex flex-row justify-between items-center text-center ">
         <ul className="flex flex-row justify-between ">
           <li className="mx-4">
             <Link className="hover:text-green-pine">
-              <p>Home</p>
+              <p className="text-xs sm:text-sm md:text-md lg:text-lg">Home</p>
             </Link>
           </li>
           <li className="mx-4">
             <Link className="hover:text-green-pine">
-              <p>About</p>
+              <p className="text-xs sm:text-sm md:text-md lg:text-lg">About</p>
             </Link>
           </li>
           <li className="mx-4">
             <Link className="hover:text-green-pine">
-              <p>My Trips</p>
+              <p className="text-xs whitespace-nowrap sm:text-sm md:text-md lg:text-lg">
+                My Trips
+              </p>
             </Link>
           </li>
         </ul>
 
         <Link>
-          <img src={profilePic} alt="ProfilePicture" className="mx-16" />
+          <img src={profilePic} alt="ProfilePicture" className="mx-2 sm:mx-2 md:mx-4 lg:mx-16" />
         </Link>
       </nav>
     </header>
