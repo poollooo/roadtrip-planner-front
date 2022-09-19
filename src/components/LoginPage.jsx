c46afbd7912d1ed1b7e21edce36463d105e323da
Paul Nizet <polonizet@gmail.com>
2022-09-19 17:14:18 +0200

    Login page : working & beautifull

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -47,32 +47,41 @@ function LoginPage(props) {
     };
 
     return (
-        <div className="LoginPage">
-            <h1>Login</h1>
-
-            <form onSubmit={handleLoginSubmit}>
-                <label>Username:</label>
-                <input
-                    type="text"
-                    name="Username"
-                    value={username}
-                    onChange={handleUsername}
-                />
-
-                <label>Password:</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={handlePassword}
-                />
-
-                <button type="submit">Login</button>
+        <div className="flex flex-col items-center text-center bg-gray-50 shadow-lg rounded-lg mx-48">
+            <h1 className="text-3xl font-bold pt-8">Welcome back !
+                <br />
+                <span className="text-lg font-normal">Log back in to start planning your next adventure</span>
+            </h1>
+            <form className="flex flex-col py-8 gap-8 mx-48" onSubmit={handleLoginSubmit}>
+                <div className="flex flex-row justify-between items-center outline-none gap-8">
+                    <label className="w-24 text-left">Username:</label>
+                    <input
+                        type="text"
+                        name="Username"
+                        value={username}
+                        onChange={handleUsername}
+                        className="border-2 border-gray-300 w-[15vw] rounded-lg px-4 py-2 focus:outline-none focus:border-green-pine"
+                    />
+                </div>
+                <div className="flex flex-row justify-between items-center outline-none gap-8">
+                    <label className="w-24 text-left">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handlePassword}
+                        className="border-2 border-gray-300 w-[15vw] rounded-lg px-4 py-2 focus:outline-none focus:border-green-pine"
+                    />
+                </div>
+                <button type="submit" className="h-12 hover:text-green-pine hover:bg-white hover:border-2 border-2 border-green-pine hover:border-green-pine text-xl bg-green-pine rounded-lg text-white ">
+                    Login
+                </button>
             </form>
             {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-            <p>Don't have an account yet?</p>
-            <Link to={"/signup"}> Sign Up</Link>
+            <div className="flex flex-row gap-2 pt-4 pb-8">
+                <p className="text-md">Don't have an account yet?</p>
+                <Link className="hover:text-green-pine text-md" to={"/signup"}> Sign Up</Link>
+            </div>
         </div>
     )
 }
