adf34a9ab6811dc1aadde69ba263acc66762d5de
Paul Nizet <polonizet@gmail.com>
2022-09-19 11:35:37 +0200

    wrap index.js with AuthContext

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Login from "./Login";
-import SignUp from "./SignUp";
+import SignupPage from "./SignupPage";
 
 const Authentication = () => {
   return (
     <>
       <Login />
-      <SignUp />
+      <SignupPage />
     </>
   );
 };
