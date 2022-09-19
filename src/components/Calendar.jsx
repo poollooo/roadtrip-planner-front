3ef2bd181e28e84126158fb6e2b57f2c39aa7c04
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-09-19 22:46:12 +0200

    rework to display real data. Added icon to the activities list

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./material.css";
+import "./event.css";
+import restIcon from "../images/restaurant-icon.png";
+import actIcon from "../images/activities-icon.png";
+
 import {
   Inject,
   ScheduleComponent,
@@ -8,67 +12,94 @@ import {
   ViewDirective,
   DragAndDrop,
   Resize,
-  CellClickEventArgs,
 } from "@syncfusion/ej2-react-schedule";
-
-import { appointments } from "./data-test";
 
 import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
 
-const Calendar = () => {
-  const myRef = useRef();
-  const dataTest = [
-    {
-      Id: 3000,
-      Subject: "Test 2",
-    },
-    {
-      Id: 3001,
-      Subject: "Test 2",
-    },
-  ];
-  const field = { dataSource: dataTest, id: "Id", text: "Subject" };
+const Calendar = (props) => {
+  // const activitiesList = props.activitiesList;
 
-  const dataSource = appointments.map(
-    ({ title, startDate, endDate }, index) => {
-      return {
-        Id: index,
-        Subject: title,
-        StartTime: startDate,
-        EndTime: endDate,
-        IsAllDay: false,
-        Status: "Completed",
-        Priority: "High",
-      };
-    }
+  const [scheduledActivities, setActivities] = useState([]);
+  const [activitiesList, setList] = useState(props.activitiesList);
+  console.log(activitiesList);
+
+  const myRef = useRef();
+  const datal = activitiesList.map(
+    ({ activityLocationId, name, category }) => ({
+      id: activityLocationId,
+      name,
+      category,
+    })
   );
   const onDragStart = (drag) => {
     drag.navigation.enable = true;
     drag.navigation.timeDelay = 1000;
   };
+  const data = {
+    dataSource: datal,
+    id: "id",
+    text: "name",
+    category: "category",
+  };
 
+  const nodeTemplate = (data) => {
+    return (
+      <div className="flex justify-start items-center gap-2 w-full font-medium text-sm">
+        <img
+          src={data.category === "restaurant" ? restIcon : actIcon}
+          alt="temp"
+          className="h-8 "
+        />
+        <p>{data.name}</p>
+      </div>
+    );
+  };
+
+  //redifining the display of activities in the calendar
+  // const templateEvent = (data) => {
+  //   return <div>{data.Subject}</div>;
+  // };
+
+  // const popupDetail = (activityData) => {
+  //   console.log(activityData.Subject);
+  //   return (
+  //     <div className="custom-event-editor">
+  //       <p>Activity name : {activityData.Subject}</p>
+  //       <p>Location :</p>
+  //     </div>
+  //   );
+  // };
+
+  //treetemplate to define
+
+  //allow to add event do to the scheduler
   const onDragStop = (event) => {
     const cellData = myRef.current.getCellDetails(event.target);
+    console.log(cellData);
     const newEvent = {
       Subject: event.draggedNodeData.text,
       StartTime: cellData.startTime,
       EndTime: cellData.endTime,
       IsAllDay: false,
     };
-
+    setActivities({
+      startDate: cellData.startTime,
+      enDate: cellData.endTime,
+      tripId: 2222,
+      activityId: event.draggedNodeData.id,
+    });
     myRef.current.openEditor(newEvent, "Add", true);
+    //myRef.current.addEvent(newEvent, true);
   };
-
+  console.log(myRef);
   return (
     <>
       <div className="w-4/6">
         <ScheduleComponent
-          selectedDate={new Date(2018, 5, 25)}
-          eventSettings={{ dataSource }}
           workHours={{
             highlight: true,
             start: "7:00",
-            end: "24:00",
+            end: "23:00",
           }}
           ref={myRef}
           dragStart={onDragStart}
@@ -90,7 +121,6 @@ const Calendar = () => {
             <ViewDirective
               option="Day"
               interval={10}
-              isSelected={true}
               displayName="All Trip"
               startHour="08:00"
               endHour="24:00"
@@ -102,13 +132,11 @@ const Calendar = () => {
       <div className="w-2/6">
         <p>Activities list </p>
         <TreeViewComponent
-          fields={field}
+          fields={data}
           allowDragAndDrop={true}
           nodeDragStop={onDragStop}
+          nodeTemplate={nodeTemplate}
         />
-        {/* {dataTest.map((e) => {
-          return <div className="h-250 bg-slate-500 border">{e.Subject}</div>;
-        })} */}
       </div>
     </>
   );
--- src/components/DisplayOneTrip.jsx
+++ src/components/DisplayOneTrip.jsx
@@ -1,27 +1,12 @@
 import React from "react";
 import Calendar from "./Calendar";
-// import {
-//   TreeViewComponent,
-//   DragAndDropEventArgs,
-// } from "@syncfusion/ej2-react-navigations";
+import { activities } from "./data-test";
 
 const DisplayOneTrip = () => {
-  //   const dataTest = [
-  //     {
-  //       Id: 3000,
-  //       Subject: "Test 2",
-  //     },
-  //     {
-  //       Id: 3001,
-  //       Subject: "Test 2",
-  //     },
-  //   ];
-  //   const field = { dataSource: dataTest, id: "Id", text: "Subject" };
-
   return (
     <div className="flex gap-[2rem]">
       {/* <div className="w-4/6"> */}
-      <Calendar></Calendar>
+      <Calendar activitiesList={activities.newActivityList}></Calendar>
       {/* </div> */}
     </div>
   );
