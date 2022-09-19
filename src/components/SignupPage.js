e3b7f3e69d7bd432b7760d5f9d4ded10c421213d
Paul Nizet <polonizet@gmail.com>
2022-09-19 17:29:27 +0200

    save work on signup page

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,7 +1,8 @@
 // src/pages/SignupPage.js
 
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
 import axios from "axios";
 
 const API_URL = "http://localhost:3003/api";
@@ -9,28 +10,38 @@ const API_URL = "http://localhost:3003/api";
 
 function SignupPage(props) {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState(undefined);
+
+    const { storeToken, authenticateUser } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
     const handleEmail = (e) => setEmail(e.target.value);
-    const handlePassword = (e) => setPassword(e.target.value);
     const handleUsername = (e) => setUsername(e.target.value);
+    const handlePassword = (e) => setPassword(e.target.value);
+    console.log(email, username, password);
 
 
     const handleSignupSubmit = (e) => {
         e.preventDefault();
         // Create an object representing the request body
-        const requestBody = { email, password, username };
+        const requestBody = { email, username, password };
+        console.log(requestBody);
 
         // Make an axios request to the API
-        // If POST request is successful redirect to login page
+        // If POST request is successful redirect to main page
         // If the request resolves with an error, set the error message in the state
         axios.post(`${API_URL}/auth/signup`, requestBody)
             .then((response) => {
-                navigate('/login');
+                // Store the JWT token in the browser's localStorage
+                console.log(response)
+                storeToken(response.data);
+                // Verify the token by sending a request 
+                // to the server's JWT validation endpoint.
+                authenticateUser();
+                navigate('/');
             })
             .catch((error) => {
                 const errorDescription = error.response.data.message;
