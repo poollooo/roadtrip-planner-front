f9bb92e464114b469b4d53fbd6255b2b5307c2cd
Paul Nizet <polonizet@gmail.com>
2022-09-21 16:19:33 +0200

    make display cards fetch cities in our database

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
-const DisplayCards = (props) => {
-  const { headerContent, trip } = props;
+const DisplayCards = ({ headerContent, trip }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [cities, setCities] = useState();
+  console.log(cities)
+
+  useEffect(() => {
+    axios.get("https://roadtrip-planner-ih.herokuapp.com/api/cities")
+      .then((response) => {
+        // If the server verifies that JWT token is valid
+        setCities(response.data.allCities)
+      })
+      .catch((error) => {
+        // If the server sends an error response (invalid token) 
+        // Update state variables         
+        console.log(error)
+      });
+  }, []);
+
 
   return (
-    <div className=" flex flex-col gap-5 w-full lg:w-fit">
-      {/* Header */}
-      <div className="flex justify-between ">
-        <p className="font-bold text-2xl">{headerContent}</p>
-        {trip && <p className="text-lg self-end">See All</p>}
-      </div>
-      {/* List of trip */}
-      <div className="flex flex-col lg:flex-row justify-start gap-7 items-center min-w-fit ">
-        <Card title="5 days to Paris" desc="Sept 2022 05 - Sept 2022 07"></Card>
-        <Card title="5 days to Paris" desc="Sept 2022 05 - Sept 2022 07"></Card>
-        <Card title="5 days to Paris" desc="Sept 2022 05 - Sept 2022 07"></Card>
+    <div className="flex flex-col w-[80vw] shadow-lg p-14 bg-offwhite-100 rounded-2xl">
+      <div className="flex flex-col gap-5">
+        {/* Header */}
+        <div className="flex justify-between">
+          <p className="font-bold text-2xl">{headerContent}</p>
+          {trip && <p className="text-lg self-end">See All</p>}
+        </div>
+        {/* List of trip */}
+        <div className="flex flex-col lg:flex-row justify-start gap-10 items-center ">
+          {cities && cities?.slice(0, 3).map((city) => {
+            return (
+              <Card
+                // add the city name with the first letter in capital
+                title={city.name.charAt(0).toUpperCase() + city.name.slice(1)}
+                desc="Sept 2022 05 - Sept 2022 07"
+                image={city.image}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
