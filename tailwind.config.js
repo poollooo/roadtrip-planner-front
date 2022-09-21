29fa421178f29a091142a35e2fcbb50deb1ecf73
Paul Nizet <polonizet@gmail.com>
2022-09-21 11:40:27 +0200

    add custom color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
           pine: "#03666B",
           night: "#07232c",
         },
+        offwhite: {
+          custom: "#FFF7E8",
+          100: "#f5f5f5",
+          200: "#eeeeee",
+        }
       },
     },
   },
