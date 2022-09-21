9219ad9b150b4d9810eb6c678ba08ad85cbefc82
Paul Nizet <polonizet@gmail.com>
2022-09-21 16:19:55 +0200

    fix card component styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import iconLoc from "../images/localisation-icon.png";
 
-const Card = (props) => {
-  const { title, desc } = props;
+const Card = ({ title, desc, image }) => {
   return (
     <div className="flex flex-col gap-1 w-80">
       {/* images */}
-      <div className="h-4/5  w-full lg:w-80">
+      <div className="">
         <img
-          src="https://www.parisinfo.com/var/otcp/sites/images/node_43/node_51/node_230/vue-a%C3%A9rienne-paris-tour-eiffel-coucher-de-soleil-|-630x405-|-%C2%A9-fotolia/19544352-1-fre-FR/Vue-a%C3%A9rienne-Paris-Tour-Eiffel-coucher-de-soleil-|-630x405-|-%C2%A9-Fotolia.jpg"
-          className="h-64 lg:h-72 object-cover rounded-xl shadow-lg shadow-gray-400/40 "
+          src={image}
+          className="h-64 lg:h-72 object-cover w-full rounded-xl shadow-lg shadow-gray-400/40 "
           alt="bla"
         />
       </div>
