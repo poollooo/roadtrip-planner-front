bd7684dab185853cb80433ec692e883d1b3d32fc
Paul Nizet <polonizet@gmail.com>
2022-09-21 11:19:09 +0200

    add PlaneLoading component to isAnon & isPrivate

--- src/components/isAnonymous.js
+++ src/components/isAnonymous.js
@@ -3,13 +3,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import { Navigate } from "react-router-dom";
+import PlaneLoading from "./PlaneLoading";
 
 function IsAnon({ children }) {
 
     const { isLoggedIn, isLoading } = useContext(AuthContext);
 
     // If the authentication is still loading 
-    if (isLoading) return <p>Loading ...</p>;
+    if (isLoading) {
+        return (
+            <PlaneLoading text={"Authenticating you in"} />
+        )
+    }
 
     if (isLoggedIn) {
         // If the user is logged in, navigate to home page     
