86127ef320b98e01601e739936a12631efc1cea1
Paul Nizet <polonizet@gmail.com>
2022-09-22 18:37:35 +0200

    set back-end to remote

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-// const ORIGIN = "https://roadtrip-planner-ih.herokuapp.com/api"
-const ORIGIN = "http://localhost:3003/api"
+const ORIGIN = "https://roadtrip-planner-ih.herokuapp.com/api"
+// const ORIGIN = "http://localhost:3003/api"
 
 export { ORIGIN }
