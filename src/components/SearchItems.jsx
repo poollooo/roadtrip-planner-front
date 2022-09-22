5a16d5f0ab28f514118c68b851d96035962cd905
Paul Nizet <polonizet@gmail.com>
2022-09-22 15:08:20 +0200

    fix searchItems styling

--- src/components/SearchItems.jsx
+++ src/components/SearchItems.jsx
@@ -14,8 +14,29 @@ const SearchItems = ({ item, isHidden, setCurrentActivity }) => {
     return;
   }
 
+  const handleSelect = () => {
+    if (!cancelSelected) {
+      setSelectedExperience((prevState) => {
+        const foundExp = prevState.find((ele) => ele._id === item._id);
+        if (foundExp) {
+          return prevState;
+        }
+        return [...prevState, item];
+      });
+    } else {
+      setSelectedExperience((prevState) => {
+        const keepExp = prevState.filter((ele) => ele._id !== item._id);
+
+        return [...keepExp];
+      });
+    }
+    setCancelSelected(!cancelSelected);
+  }
+
   return (
-    <div id={item._id} className={`SearchItem ${isHidden && "hidden"}`}>
+    <div id={item._id}
+      className={`SearchItem ${isHidden && "hidden"}`}
+    >
       <picture>
         <img
           onClick={() => {
@@ -23,43 +44,40 @@ const SearchItems = ({ item, isHidden, setCurrentActivity }) => {
           }}
           src={item.photo}
           alt={`${item.category} View`}
+          className="rounded-lg px-0 w-12"
         />
-        <img
-          src={!cancelSelected ? addSvg : addedSvg}
-          alt="add-experience"
-          className="icon-add"
-          onClick={() => {
-            if (!cancelSelected) {
-              setSelectedExperience((prevState) => {
-                const foundExp = prevState.find((ele) => ele._id === item._id);
-                if (foundExp) {
-                  return prevState;
-                }
-                return [...prevState, item];
-              });
-            } else {
-              setSelectedExperience((prevState) => {
-                const keepExp = prevState.filter((ele) => ele._id !== item._id);
-
-                return [...keepExp];
-              });
-            }
-            setCancelSelected(!cancelSelected);
-          }}
-        />
+        <button>
+          <img
+            src={!cancelSelected ? addSvg : addedSvg}
+            alt="add-experience"
+            className="icon-add"
+            onClick={handleSelect}
+          />
+        </button>
       </picture>
+      <div className="flex items-end w-full justify-between">
+        <div className="flex flex-col items-start mt-1 justify-start">
+          <h3>
+            <strong>{item.name.length > 24 ? item.name.slice(0, 24) + '...' : item.name}</strong>
+          </h3>
 
-      <div className="Search-item-description">
-        <h3>
-          <strong>{item.name}</strong>
-        </h3>
-
-        <p>
-          Rating : {Number(item.rawRating).toFixed(1)} 🌟
-        </p>
-        <p>
-          Numbers Of Views : {item.numberOfReviews}
-        </p>
+          <p>
+            Rating : {Number(item.rawRating).toFixed(1)} 🌟
+          </p>
+          <p>
+            Numbers Of Views : {item.numberOfReviews}
+          </p>
+        </div>
+        <div>
+          <button
+            className="bg-green-pine text-white rounded-md w-14 border-2 border-green-pine"
+            // switch className if the item is selected
+            {...(cancelSelected && { className: "bg-white text-green-pine border-2 border-green-pine rounded-md w-20" })}
+            onClick={handleSelect}
+          >
+            {!cancelSelected ? 'Add' : 'Remove'}
+          </button>
+        </div>
       </div>
     </div>
   );
